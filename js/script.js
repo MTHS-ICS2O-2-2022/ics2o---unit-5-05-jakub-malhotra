4a4166c765969c6871109d3b4be68d9506dfa78b
jakub-malhotra <124389450+jakub-malhotra@users.noreply.github.com>
2023-05-06 19:33:46 +0000

    initial commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,55 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jakub Malhotra
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function myButtonClicked() {
+  const lengthA = parseFloat(document.getElementById("length-a").value)
+  const lengthB = parseFloat(document.getElementById("length-b").value)
+  const lengthC = parseFloat(document.getElementById("length-c").value)
+
+  // using the cosine law
+  const angleA =
+    Math.acos(
+      (lengthB ** 2 + lengthC ** 2 - lengthA ** 2) / (2 * lengthB * lengthC)
+    ) *
+    (180 / Math.PI)
+  const angleB =
+    Math.acos(
+      (lengthC ** 2 + lengthA ** 2 - lengthB ** 2) / (2 * lengthC * lengthA)
+    ) *
+    (180 / Math.PI)
+  const angleC =
+    Math.acos(
+      (lengthA ** 2 + lengthB ** 2 - lengthC ** 2) / (2 * lengthA * lengthB)
+    ) *
+    (180 / Math.PI)
+
+  const sumOfAngles =
+    Number(angleA.toFixed(2)) +
+    Number(angleB.toFixed(2)) +
+    Number(angleC.toFixed(2))
+
+  console.log(angleA)
+  console.log(angleB)
+  console.log(angleC)
+  console.log(sumOfAngles)
+
+  if (sumOfAngles == 180) {
+    if (lengthA == lengthC && lengthA == lengthB && lengthB == lengthC) {
+      document.getElementById("answer").innerHTML =
+        "This is an equilateral triangle."
+    } else if (lengthA == lengthC || lengthC == lengthB || lengthA == lengthB) {
+      document.getElementById("answer").innerHTML =
+        "This is an isosceles triangle."
+    } else if (lengthA != lengthC && lengthA != lengthB && lengthB != lengthC) {
+      document.getElementById("answer").innerHTML =
+        "This is a scalene triangle."
+    }
+  } else {
+    document.getElementById("answer").innerHTML = "This is not a Triangle. (Sum of angles is not 180°)"
+  }
+}
